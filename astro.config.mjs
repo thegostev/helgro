5db8e8d33a19186574c4d43ad6b11a1a24355ba4
Michael Andreuzza <34077176+michael-andreuzza@users.noreply.github.com>
2025-12-19 18:24:26 +0200

    Refactor astro.config.mjs and update site URL
    Removed markdown and shikiConfig settings, updated site URL.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,17 +6,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  markdown: {
-    drafts: true,
-    shikiConfig: {
-      theme: "css-variables",
-    },
-  },
-  shikiConfig: {
-    wrap: true,
-    skipInline: false,
-    drafts: true,
-  },
-  site: "https://lexingtonthemes.com",
+  site: "https://yourwebsite.com",
   integrations: [tailwindcss(), sitemap(), mdx()],
 });
